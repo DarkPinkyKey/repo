1bb6acfd4a6aa488d263e8f3aa2c7f15f5631e68
Keyla <134055890+DarkPinkyKey@users.noreply.github.com>
2023-07-13 12:24:02 +0200

    Ejercicio diapo 186 JAVA

---
+++ prueba/Excercice_diapo186.java
@@ -0,0 +1,22 @@
+package prueba;
+
+public class Excercice_diapo186 {
+	
+	public static void main(String[] args) {
+		String nombre = "Keyla";
+		String apellido = "Batzin";
+		String calle = "Castillejos";
+		String municipio = "Barcelona";
+		String codigoPostal = "01516";
+		char sexo = 'F';
+		String correoElectronico = "keylabatzin@gmail.com";
+		
+		System.out.println("Nombre:\t" + nombre);
+		System.out.println("Apellido:\t" + apellido);
+		System.out.println("Calle:\t" + calle);
+		System.out.println("Municipio:\t" + municipio);
+		System.out.println("Codigo\tPostal:\t" + codigoPostal);
+		System.out.println("Sexo:\t" + sexo);
+		System.out.println("Correo\tElectronico:\t" + correoElectronico);
+	}
+}
