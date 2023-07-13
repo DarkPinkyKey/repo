687f70ade7c609c1530b8488392ef4799c15fee2
Keyla <134055890+DarkPinkyKey@users.noreply.github.com>
2023-07-13 12:45:00 +0200

    Ejercicio diapo 196 JAVA

---
+++ prueba/Excercice_diapo196.java
@@ -0,0 +1,25 @@
+package prueba;
+
+public class Excercice_diapo196 {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 5;
+		int num3 = 55;
+		int num4 = 45;
+		int num5 = 3;
+		int num6 = 25;
+		int num7 = 7;
+		float result = (float) num6 / num7;
+		int num8 = 4;
+		
+		System.out.println(num1-num2); //Resta//
+		System.out.println(num3+num4); //Suma//
+		System.out.println(-num5); //Transformar numero en negativo//
+		System.out.println(num2*num2); //Multipicacion//
+		System.out.println(num6/num7); //Division entera//
+		System.out.println(result); //Division numero real//
+		System.out.println(num6%num8); //Resto de dividir 25/4 //
+	}
+
+}
