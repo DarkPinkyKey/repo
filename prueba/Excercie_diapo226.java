6588ed68cc2c7f9c0d5c83996e65343fa1f0a913
Keyla <134055890+DarkPinkyKey@users.noreply.github.com>
2023-07-14 10:45:59 +0200

    Ejercicios JAVA

---
+++ prueba/Excercie_diapo226.java
@@ -0,0 +1,29 @@
+package prueba;
+
+public class Excercie_diapo226 {
+
+	public static void main(String[] args) {
+		int numero = 5; //Asignacion de número //
+		
+		String resultado = (numero % 2 == 0)?"par":"impar";
+		
+		System.out.println("Es"+" "+ resultado);
+	}
+
+}
+
+-------------------
+package prueba;
+
+public class Excercie_diapo226 {
+
+	  public static void main(String[] args) {
+	        boolean resultado = Math.random() < 0.5;
+
+	        String color = resultado ? "blanco" : "negro";
+
+	        System.out.println(color);
+
+	}
+
+}
